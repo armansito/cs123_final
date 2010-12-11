ca0f49337a1df41a5c8b6bed70b77374394f330e
Arman Uguray <auguray@cs.brown.edu>
2010-12-10 23:35:55 -0500

    shouldn't bother us about settingsChanged

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -9,9 +9,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
 
-#define BIND(b) { DataBinding *_b = (b); connect(_b, SIGNAL(dataChanged()), this, SLOT(settingsChanged())); }
+    #define BIND(b) { DataBinding *_b = (b); }//connect(_b, SIGNAL(dataChanged()), this, SLOT(settingsChanged())); }
 
-    BIND( FloatBinding::bindSliderAndTextbox(ui->amplitudeSlider, ui->amplitudeDisplay, settings.ripple_amplitude, 0, 96) )
+    BIND( FloatBinding::bindSliderAndTextbox(ui->amplitudeSlider, ui->amplitudeDisplay, settings.ripple_amplitude, 0, 3) )
     BIND( FloatBinding::bindSliderAndTextbox(ui->energySlider, ui->energyDisplay, settings.ripple_energy, 0, 96) )
     BIND( FloatBinding::bindSliderAndTextbox(ui->speedSlider, ui->speedDisplay, settings.ripple_speed, 0, 96) )
 }
