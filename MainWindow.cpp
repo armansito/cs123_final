8bd023306f2dc0c5977809465ad82cbffc032467
Arman Uguray <auguray@cs.brown.edu>
2010-12-10 23:37:26 -0500

    one more

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -11,7 +11,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     #define BIND(b) { DataBinding *_b = (b); }//connect(_b, SIGNAL(dataChanged()), this, SLOT(settingsChanged())); }
 
-    BIND( FloatBinding::bindSliderAndTextbox(ui->amplitudeSlider, ui->amplitudeDisplay, settings.ripple_amplitude, 0, 3) )
+    BIND( FloatBinding::bindSliderAndTextbox(ui->amplitudeSlider, ui->amplitudeDisplay, settings.ripple_amplitude, 0.1, 3) )
     BIND( FloatBinding::bindSliderAndTextbox(ui->energySlider, ui->energyDisplay, settings.ripple_energy, 0.1, 5) )
     BIND( FloatBinding::bindSliderAndTextbox(ui->speedSlider, ui->speedDisplay, settings.ripple_speed, 0.1, 5) )
 }
